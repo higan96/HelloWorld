cf31f67f66107b5781938df4db8221d6bcbdf58a
higan96 <norihiko.oba@gmail.com>
2024-04-25 22:18:29 +0900

    コメントを追加

--- Sources/HelloWorld/HelloWorldMan.swift
+++ Sources/HelloWorld/HelloWorldMan.swift
@@ -3,13 +3,30 @@
 
 import Foundation
 
+/// HelloWorldManは「Hello World」と言うことができる
+/// 声の大きさもシチュエーションによって使い分けることができる
 public struct HelloWorldMan {
+
+    /// HelloWorldManの名前
     private(set) var name: String
 
+    /// `name`を持つHelloWorldManを作り出す
+    ///
+    /// - Parameter name: HelloWorldManの名前
+    ///
+    /// - Note: HelloWorldManの生年月日は不明である
+    ///
     public init(name: String) {
         self.name = name
     }
 
+    /// HelloWorldManが`HelloWorld`と言う
+    ///
+    /// 声の大きさを選べるので美術館など静かな場所でも安心して`HelloWorld`と言える
+    ///
+    /// - Parameter voiceKind: どのような声で言うかを選べる
+    /// - SeeAlso: `VoiceKind`
+    ///
     public func sayHelloWorld(with voiceKind: VoiceKind = .normalVoice) -> String {
         let man = "\(self.name) < "
         let message = "Hello World"
--- Sources/HelloWorld/VoiceKind.swift
+++ Sources/HelloWorld/VoiceKind.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 
+/// 声の種類
 public enum VoiceKind {
+    /// 小さな声
     case smallVoice
+    /// 普通の声
     case normalVoice
+    /// 大きな声
     case loudVoice
 }
